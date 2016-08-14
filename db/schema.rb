024df718dde0df24d1c8c7668d4ccd8a8586c397
reinoptland <reinoptland@gmail.com>
2016-08-14 16:14:35 +0200

    Define relations Song/Artist and Create jointable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160814123445) do
+ActiveRecord::Schema.define(version: 20160814141252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 20160814123445) do
     t.string   "country_of_origin"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+  end
+
+  create_table "artists_songs", id: false, force: :cascade do |t|
+    t.integer "artist_id", null: false
+    t.integer "song_id",   null: false
   end
 
   create_table "songs", force: :cascade do |t|
