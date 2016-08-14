739cf1d86d9ea1a0012d8e9b8960aa8c65ab74f5
reinoptland <reinoptland@gmail.com>
2016-08-14 16:04:43 +0200

    Edit tests for song and artists models

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
-  it "an artist has a name"
-  it "an artist has a date of birth"
-  it "an artist has country of origin"
+  it "is valid with valid attributes" do
+    expect(Artist.new).to be_valid
+  end
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  it "a song has a title"
-  it "a song has a duration"
+  it "is valid with valid attributes"do
+    expect(Song.new).to be_valid
+  end
 end
