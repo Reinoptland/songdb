e1707128a2b4b32b18e831cbc06b3fd0f3f315a7
reinoptland <reinoptland@gmail.com>
2016-08-28 17:13:53 +0200

    Add viewtemplate for artist show page

--- spec/features/show_artist_spec.rb
+++ spec/features/show_artist_spec.rb
@@ -4,9 +4,9 @@ describe 'It shows the artist\'s details' do
 
   it "shows the artist\'s details" do
 
-  gza = Artist.create(name: "GZA", birth_date:"1966-08-22", country_of_origin: "United States",)
+  gza = Artist.create(id: 10, name: "GZA", birth_date:"1966-08-22", country_of_origin: "United States",)
 
-  visit "http://example.com/artist/#{gza.id}"
+  visit "http://example.com/artists/10"
 
   expect(page).to have_text(gza.name)
   expect(page).to have_text(gza.birth_date)
