1d84e0eefb45fee922d907b5427916d5ceea986f
reinoptland <reinoptland@gmail.com>
2016-08-28 17:04:45 +0200

    Add route and controlleraction for artist#show

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -6,6 +6,9 @@ class ArtistsController < ApplicationController
   def order_by_name
     @artists = Artist.order_by_name
   end
+  def show
+    @artist = Artist.find(params[:id])
+  end
 
   private
 
