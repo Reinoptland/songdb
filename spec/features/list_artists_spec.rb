f17e6b17777de4cd82f366fa14b7795962e51432
reinoptland <reinoptland@gmail.com>
2016-08-28 13:25:49 +0200

    Add order_by_name test to list_artist spec

--- spec/features/list_artists_spec.rb
+++ spec/features/list_artists_spec.rb
@@ -17,6 +17,24 @@ describe 'Visitors can see a list of all the artists in the database' do
       expect(page).to have_text(mm.name)
     end
 
+    it 'has a link to order the artist by name' do
+      gza = Artist.create(name: "GZA", birth_date:"1966-08-22", country_of_origin: "United States",)
+      rza = Artist.create(name: "RZA", birth_date:"1969-07-05", country_of_origin: "United States",)
+      mm = Artist.create(name: "Method Man", birth_date:"1971-03-02", country_of_origin: "United States",)
+
+      visit artists_url
+
+      expect(page.find('h1:nth-child(1)')).to have_content 'GZA'
+      expect(page.find('h1:nth-child(4)')).to have_content 'RZA'
+      expect(page.find('h1:nth-child(7)')).to have_content 'Method Man'
+
+      click_link('order_by_created_at')
+
+      expect(page.find('h1:nth-child(1)')).to have_content 'GZA'
+      expect(page.find('h1:nth-child(4)')).to have_content 'Method Man'
+      expect(page.find('h1:nth-child(7)')).to have_content 'RZA'
+    end
+
     it 'has a link to order the artists by created_at' do
     end
 end
