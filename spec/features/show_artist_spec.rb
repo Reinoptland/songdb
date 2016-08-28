2c1d2aad5488fcc2bf8733fcbf17c160358bdfc1
reinoptland <reinoptland@gmail.com>
2016-08-28 17:25:46 +0200

    Write list artists songs test in artist_show spec

--- spec/features/show_artist_spec.rb
+++ spec/features/show_artist_spec.rb
@@ -4,13 +4,29 @@ describe 'It shows the artist\'s details' do
 
   it "shows the artist\'s details" do
 
-  gza = Artist.create(id: 10, name: "GZA", birth_date:"1966-08-22", country_of_origin: "United States",)
+    gza = Artist.create(id: 10, name: "GZA", birth_date:"1966-08-22", country_of_origin: "United States",)
 
-  visit "http://example.com/artists/10"
+    visit "http://example.com/artists/10"
 
-  expect(page).to have_text(gza.name)
-  expect(page).to have_text(gza.birth_date)
-  expect(page).to have_text(gza.country_of_origin)
+    expect(page).to have_text(gza.name)
+    expect(page).to have_text(gza.birth_date)
+    expect(page).to have_text(gza.country_of_origin)
+
+  end
+
+  it "shows the artist\'s songs" do
+    gza = Artist.create(id: 10, name: "GZA", birth_date:"1966-08-22", country_of_origin: "United States",)
+
+    Song.create([
+    {title:"Clan in da Front", duration: 273, artists: [gza]},
+    {title:"Bring da Ruckus", duration: 250, artists: [gza]},
+    ])
+
+    visit "http://example.com/artists/10"
+
+    expect(page).to have_text(gza.songs.first.title)
+    expect(page).to have_text(gza.songs.second.title)
+    expect(page).to have_text(gza.songs.first.duration)
 
   end
 end
